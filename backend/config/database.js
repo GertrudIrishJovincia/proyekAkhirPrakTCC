ae9255ba9112724812c296ee984a3c505c1e2d8f
Irish Jovincia <irishjovinciaa@gmail.com>
2025-05-27 03:45:25 +0700

    data

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,24 +1,20 @@
+// backend/config/database.js
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
-
 dotenv.config();
 
 const db = new Sequelize(
-  process.env.DB_NAME || "hotels",
-  process.env.DB_USER || "root",
-  process.env.DB_PASSWORD || "dbhotels",
-  {
-    host: process.env.DB_HOST || "34.56.122.248",
-    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 3306,
-    dialect: "mysql",
-    logging: process.env.NODE_ENV === "development" ? console.log : false,
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000,
-    },
-  }
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+        host: process.env.DB_HOST,
+        dialect: "mysql",
+        logging: false,
+        define: {
+            timestamps: false
+        }
+    }
 );
 
-export default db;
+export default db;
